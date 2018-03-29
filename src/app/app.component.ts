8a44e83206a0c8c5d45457371fe1dcf0204e334e
cehager <charlie80088@gmail.com>
2018-03-29 11:15:36 -0600

    Added navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AlertifyService } from './services/alertify.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'dbArmor';
+
+  constructor(private alertify: AlertifyService) {}
+
+  ngOnInit() {
+    this.alertify.success('Welcome to dbArmor again!');
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
+import {HttpModule} from '@angular/http';
+import {FormsModule} from '@angular/forms';
+import {AlertifyService} from './services/alertify.service';
+import {BsDropdownModule} from 'ngx-bootstrap';
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
-  ],
+    AppComponent,
+    NavComponent
+],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule,
+    FormsModule,
+    BsDropdownModule.forRoot()
   ],
-  providers: [],
+  providers: [AlertifyService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
